97f007fc8086bcda4ddb7e7f0c7000f7daa6b7a4
rakib13x <rakib13x@gmail.com>
2024-02-25 01:21:05 +0600

    admin middleware added to check if it's a admin or user

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,8 +1,8 @@
 import express from "express";
 import { registerUser } from "../controllers/userController";
-import { protect } from "../middleware/authMiddleware";
+import { protect, admin } from "../middleware/authMiddleware";
 
 const router = express.Router();
-router.route("/").post(registerUser).get(protect, getUsers);
+router.route("/").post(registerUser).get(protect, admin, getUsers);
 
 export default router;
