ef7737acde572b5f8d5f265003c2279c8a8d6d0c
rakib13x <rakib13x@gmail.com>
2024-02-25 00:54:33 +0600

    register user api added

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,1 +1,7 @@
+import express from "express";
+import { registerUser } from "../controllers/userController";
 
+const router = express.Router();
+router.route("/").post(registerUser);
+
+export default router;
