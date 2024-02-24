0a36c1ffc09e11562a7bbcb5d92eb1033658e2ed
rakib13x <rakib13x@gmail.com>
2024-02-25 00:57:20 +0600

    protected middleware added

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,7 +1,8 @@
 import express from "express";
 import { registerUser } from "../controllers/userController";
+import { protect } from "../middleware/authMiddleware";
 
 const router = express.Router();
-router.route("/").post(registerUser);
+router.route("/").post(registerUser).get(protect, getUsers);
 
 export default router;
