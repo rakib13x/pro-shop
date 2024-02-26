93ebf4591d6944f61a0c79aa3d7172988d611002
rakib13x <rakib13x@gmail.com>
2024-02-26 13:49:13 +0600

    authUser added for user Authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,26 @@ import asyncHandler from "../middleware/asyncHandler.js";
 import generateToken from "../utils/generateToken.js";
 import User from "../models/userModel.js";
 
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
+});
+
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
 
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { registerUser } from "../controllers/userController";
+import { registerUser, authUser } from "../controllers/userController";
 import { protect, admin } from "../middleware/authMiddleware";
 
 const router = express.Router();
 router.route("/").post(registerUser).get(protect, admin, getUsers);
-
+router.post("/auth", authUser);
 export default router;
