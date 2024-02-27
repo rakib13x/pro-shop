f8a81ba15a2801483ccf31f1f23e59126b3e742c
rakib13x <rakib13x@gmail.com>
2024-02-27 15:40:17 +0600

    api added for getUserProfile in backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -53,4 +53,20 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { registerUser };
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+export { registerUser, authUser, getUserProfile };
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,8 +1,13 @@
 import express from "express";
-import { registerUser, authUser } from "../controllers/userController";
+import {
+  registerUser,
+  authUser,
+  getUserProfile,
+} from "../controllers/userController";
 import { protect, admin } from "../middleware/authMiddleware";
 
 const router = express.Router();
 router.route("/").post(registerUser).get(protect, admin, getUsers);
 router.post("/auth", authUser);
+router.route("/profile").get(protect, getUserProfile);
 export default router;
