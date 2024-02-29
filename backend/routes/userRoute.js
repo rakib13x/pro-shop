b40abfbd3d306ab7ea55196f5db7bbad2503be13
rakib13x <rakib13x@gmail.com>
2024-02-29 21:47:04 +0600

    updateUserProfile Api added

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -2,11 +2,13 @@ import express from "express";
 import {
   registerUser,
   authUser,
+  logoutUser,
   getUserProfile,
   getUsers,
   getUserById,
   updateUser,
   deleteUser,
+  updateUserProfile,
 } from "../controllers/userController";
 import { protect, admin } from "../middleware/authMiddleware";
 
@@ -14,7 +16,10 @@ const router = express.Router();
 router.route("/").post(registerUser).get(protect, admin, getUsers);
 router.post("/auth", authUser);
 router.post("/logout", logoutUser);
-router.route("/profile").get(protect, getUserProfile);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 router
   .route("/:id")
   .delete(protect, admin, deleteUser)
