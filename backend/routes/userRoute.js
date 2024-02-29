55ce396bf7528f68cdd3be9f554357bcb86d6983
rakib13x <rakib13x@gmail.com>
2024-02-29 20:54:45 +0600

    updateUser api added

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -5,6 +5,7 @@ import {
   getUserProfile,
   getUsers,
   getUserById,
+  updateUser,
 } from "../controllers/userController";
 import { protect, admin } from "../middleware/authMiddleware";
 
@@ -13,6 +14,9 @@ router.route("/").post(registerUser).get(protect, admin, getUsers);
 router.post("/auth", authUser);
 router.post("/logout", logoutUser);
 router.route("/profile").get(protect, getUserProfile);
-router.route("/:id").get(protect, admin, getUserById);
+router
+  .route("/:id")
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser);
 
 export default router;
