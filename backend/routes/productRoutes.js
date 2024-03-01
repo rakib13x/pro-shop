6b6fea33b9715ad42cf0cd711eeef83840a25b11
rakib13x <rakib13x@gmail.com>
2024-03-01 23:24:14 +0600

    getProducts api  Added with pagination and search functionality

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,4 +1,6 @@
 import express from "express";
 const router = express.Router();
-import {} from "../controllers/productController.js";
+import { getProducts } from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
+
+router.route("/").get(getProducts);
