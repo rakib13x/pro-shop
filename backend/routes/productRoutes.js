c276f76f2fe54a40babae077859abf89bba8f880
rakib13x <rakib13x@gmail.com>
2024-03-02 16:14:51 +0600

    updateProduct  api created and some problem fixed

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,9 +4,13 @@ import {
   getProducts,
   createProduct,
   getProductById,
+  updateProduct,
 } from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
 router.route("/").get(getProducts).post(protect, admin, createProduct);
 
-router.route("/:id").get(checkObjectId, getProductById);
+router
+  .route("/:id")
+  .get(checkObjectId, getProductById)
+  .put(protect, admin, checkObjectId, updateProduct);
