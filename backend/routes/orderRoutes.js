c8972b94e40308eb085f1d54e7a00703990390cc
rakib13x <rakib13x@gmail.com>
2024-03-03 22:37:20 +0600

    paypal.js for payment and and updateOrderToPaid  api added

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -5,11 +5,12 @@ import {
   getOrders,
   getMyOrders,
   getOrderById,
+  updateOrderToPaid,
 } from "../controllers/orderController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
 router.route("/").post(protect, addOrderItems).get(protect, admin, getOrders);
 router.route("/mine").get(protect, getMyOrders);
 router.route("/:id").get(protect, getOrderById);
-
+router.route("/:id/pay").put(protect, updateOrderToPaid);
 export default router;
