fe1d8d400d296c1d768d28732c4de2137454e58f
rakib13x <rakib13x@gmail.com>
2024-03-08 06:37:14 +0600

    searchbox added

--- frontend/src/components/SearchBox.jsx
+++ frontend/src/components/SearchBox.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
 
 const SearchBox = () => {
-  return <div>SearchBox</div>;
+  return (
+    <Form className="d-flex">
+      <Form.Control
+        type="text"
+        name="q"
+        placeholder="Search Products..."
+        className="mr-sm-2 ml-sm-5"
+      ></Form.Control>
+      <Button type="submit" variant="outline-success" className="p-2 mx-2">
+        Search
+      </Button>
+    </Form>
+  );
 };
 
 export default SearchBox;
