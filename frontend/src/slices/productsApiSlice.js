1f9a40a001595bc771360d0f5bcdcaaa6f629e7d
rakib13x <rakib13x@gmail.com>
2024-03-09 16:42:13 +0600

    HomeScreen with ProductCarousel added

---
+++ frontend/src/slices/productsApiSlice.js
@@ -0,0 +1,40 @@
+import { PRODUCTS_URL } from "../constant";
+import { apiSlice } from "./apiSlice";
+
+export const productsApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getProducts: builder.query({
+      query: ({ keyword, pageNumber }) => ({
+        url: PRODUCTS_URL,
+        params: { keyword, pageNumber },
+      }),
+      keepUnusedDataFor: 5,
+      providesTags: ["Products"],
+    }),
+    createProduct: builder.mutation({
+      query: () => ({
+        url: `${PRODUCTS_URL}`,
+        method: "POST",
+      }),
+      invalidatesTags: ["Product"],
+    }),
+    deleteProduct: builder.mutation({
+      query: (productId) => ({
+        url: `${PRODUCTS_URL}/${productId}`,
+        method: "DELETE",
+      }),
+      providesTags: ["Product"],
+    }),
+    getTopProducts: builder.query({
+      query: () => `${PRODUCTS_URL}/top`,
+      keepUnusedDataFor: 5,
+    }),
+  }),
+});
+
+export const {
+  useGetProductsQuery,
+  useCreateProductMutation,
+  useDeleteProductMutation,
+  useGetTopProductsQuery,
+} = productsApiSlice;
