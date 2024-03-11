91ed6393af8b5fedea12024f8ee4194fef7c95b1
rakib13x <rakib13x@gmail.com>
2024-03-11 16:50:23 +0600

    cartScreen added

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,6 +5,7 @@ import authReducer from "./slices/authSlice";
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
+    cart: cartSliceReducer,
     auth: authReducer,
   },
   middleware: (getDefaultMiddleware) =>
